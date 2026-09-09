f9ef18b92df708c09a6efaa0cf323cb3dfba6a07
huyxuan264 <huyxuan264@gmail.com>
2026-09-09 04:05:14 +0000

    [WM] Script 'f/api_phieuthu/model_loader' deployed

--- f/api_phieuthu/model_loader.py
+++ f/api_phieuthu/model_loader.py
@@ -1,7 +1,7 @@
 import wmill
 from typing import Dict, Any, Optional
 
-DEFAULT_MODEL_PATH = "f/information_extraction/qwen"
+DEFAULT_MODEL_PATH = "f/api_phieuthu/qwen_vlm"
 
 
 def get_model_config(model_path: Optional[str] = None) -> Dict[str, Any]:
